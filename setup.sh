31520ddde2204d384fd155672ae193cc1fce9008
Sam Wolfs <be.samwolfs@gmail.com>
2024-03-23 09:50:34 +0100

    Switch to ArgoCD Helm Chart

--- setup.sh
+++ setup.sh
@@ -8,9 +8,7 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/mast
 
 # Install ArgoCD
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# Add ArgoCD Helm repository
+helm repo add argo https://argoproj.github.io/argo-helm
 
-# Setup ingress: https://argo-cd.readthedocs.io/en/stable/operator-manual/ingress/#kubernetesingress-nginx
-kubectl apply -n argocd -f argocd/ingress.yaml
-
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo | xclip -selection c
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d | xclip -selection c
